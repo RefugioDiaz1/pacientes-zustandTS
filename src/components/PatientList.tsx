1f2813db07fe3f751701fdfa9d45a28c8d277111
Refugio de la O Diaz <kdiaz@ahuemex.onmicrosoft.com>
2026-02-10 08:34:34 -0600

    Ultimos cambios, Zustand, persist, toast, se usaron en el proyecto

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { usePatientStore } from "../store"
+import PatientDetails from "./PatientDetails"
 
 export default function PatientList() {
+
+    const patients = usePatientStore(state=>state.patients)
+  
+
   return (
-    <div>PatientList</div>
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+        {patients.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">
+                        <p className="text-xl mt-5 mb-10 text-center">
+                            Administra tus {''}
+                            <span className="text-indigo-600 font-bold">
+                                Pacientes y Citas
+                            </span>
+                        </p>
+                    </h2>
+                    {patients.map(item => (
+
+                        <PatientDetails key={item.id}
+                        patient={item}/>
+                    ))}
+                </>
+
+        ) : (
+            <>
+                <h2 className="font-black text-3xl text-center">
+                    No hay Pacientes
+                </h2>
+                <p className="text-xl mt-5 mb-10 text-center">
+                    Comienza Agregando Pacientes {''}
+                    <span className="text-indigo-600 font-bold">
+                        y aparecerán en este lugar
+                    </span>
+                </p>
+
+            </>
+
+        )}
+    </div>
   )
 }
